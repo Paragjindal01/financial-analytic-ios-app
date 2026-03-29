1a2d6621656650676c4a3e1f545d49c631f83cf3
Parag Jindal <parag.jindal@myokanagan.bc.ca>
2026-03-29 14:30:42 -0700

    Initial Commit

---
+++ FinancialAnalytic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinancialAnalytic
+//
+//  Created by Parag Jindal on 2026-03-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinancialAnalytic/FinancialAnalyticApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialAnalyticApp.swift
+//  FinancialAnalytic
+//
+//  Created by Parag Jindal on 2026-03-29.
+//
+
+import SwiftUI
+
+@main
+struct FinancialAnalyticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
